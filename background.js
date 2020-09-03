dc18e09b246b731bd39a5893c694ad344fc3c520
3Samourai <68392445+3Samourai@users.noreply.github.com>
2020-09-03 17:42:23 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,28 +1,17 @@
 var def = 0;
 function onExecuted(result)
 {
-    var fullHd = result[0]["hd1080"]["video/mp4"]["url"];
-    var medium = result[0]["medium"]["video/mp4"]["url"];
-    var hd = result[0]["hd720"]["video/mp4"]["url"];
-    console.log(result);
-    if (def = "fullHd")
-  {
-     
-    console.log(fullHd);
-   /*
-   
-    */
-      browser.downloads.download({ url : fullHd, saveAs : true, filename : "SimplyDownloader.mp4" })
-  }
-    else if(def = "medium")
-    {
-        //console.log(medium);
-        browser.downloads.download({ url : medium, saveAs : true, filename : "SimplyDownloader.mp4" })
-    }
-    else if(def = "hd")
-    {
+    var medium = result[0][0].url;
+    var hd = result[0][1].url;
+if(def = "hd")
+    {  
         //console.log(hd);
         browser.downloads.download({ url : hd, saveAs : true, filename : "SimplyDownloader.mp4" })
+    }
+    else
+    {
+             //console.log(medium);
+             browser.downloads.download({ url : medium, saveAs : true, filename : "SimplyDownloader.mp4" })
     }
 }
 function onError(error)
