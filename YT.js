65485646a8cb534a169d43c06f9b7a4cacbdcdc3
3Samourai <68392445+3Samourai@users.noreply.github.com>
2020-09-03 17:39:57 +0200

    Update YT.js

--- YT.js
+++ YT.js
@@ -1,27 +1,12 @@
-//This is not my code it comes from 
-//Source: (https://gist.github.com/geuis/8b1b2ea57d7f9a9ae22f80d4fbf5b97f)
-
+//Create by (https://gist.github.com/geuis/8b1b2ea57d7f9a9ae22f80d4fbf5b97f)
+//Modify by 3Samourai
 (async () => {
-    const html = await fetch(window.location.href).then((resp) => resp.text()).then((text) => text);
-    const startStr = 'ytplayer.config = {';
-    const start = html.indexOf(startStr) + startStr.length - 1;
-    const end = html.indexOf('};', start) + 1;
-    const playerObj = JSON.parse(html.slice(start, end));
-  
-    playerObj.args.player_response = JSON.parse(playerObj.args.player_response);
-  
-    const videoUrls = playerObj.args.player_response.streamingData.adaptiveFormats.reduce((acc, item) => {
-      if (!acc[item.quality]) {
-        acc[item.quality] = {};
-      }
-  
-      const mimeType = item.mimeType.split(';')[0];
-  
-      acc[item.quality][mimeType] = item;
-  
-      return acc;
-    }, {});
-  
-    console.log('!!', videoUrls);
-    return(videoUrls);
-  })();
+  html = await fetch(window.location.href).then(resp => resp.text()).then(text => text);
+  startStr = 'ytplayer.config = {';
+  start = html.indexOf(startStr) + startStr.length - 1;
+  end = html.indexOf('};', start) + 1;
+  playerObj = JSON.parse(html.slice(start, end));
+  playerObj.args.player_response = JSON.parse(playerObj.args.player_response);
+  videoUrls = playerObj.args.player_response.streamingData.formats;
+  return videoUrls;
+})();
